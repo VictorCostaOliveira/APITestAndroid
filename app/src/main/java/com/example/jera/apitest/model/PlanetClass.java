73647ca43dfba8ac9ce08fec42af015cd040cb94
VictorCostaOliveira <victorcosta@jera.com.br>
2017-05-10 18:11:50 -0400

    Add api request

--- app/src/main/java/com/example/jera/apitest/model/PlanetClass.java
+++ app/src/main/java/com/example/jera/apitest/model/PlanetClass.java
@@ -1,8 +1,24 @@
 package com.example.jera.apitest.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
 /**
  * Created by jera on 09/05/17.
  */
 
 public class PlanetClass {
+
+    private int id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("population")
+    @Expose
+    private String population;
+
+
 }
